3428c30aaa13df64769a32621d51a7292e0caade
degerock <48260413+degerock@users.noreply.github.com>
2020-03-13 19:41:26 +0300

    add,fix,remove,update,change
    bla bla python

---
+++ index.py
@@ -0,0 +1,35 @@
+from course_by import generated_prophecies
+from datetime import datetime as dt
+
+def generate_page(head,body):
+    page = f"<html>{head}{body}</html>"
+    return page
+
+def generate_head(title):
+    head = f"""<meta charset='utf-8'>
+     <title>{title}</title>
+     </head>
+     """
+    return f"<head>{head}</head>"
+
+def generate_body(header, paragraphs):
+    body = f"<h1>{header}</h1>"
+    for p in paragraphs:
+        body = body + f"<p>{p}</p>"
+    return f"<body>{body}</body>"
+
+def save_page(title, header, paragraphs, output = "index.html"):
+    fp = open(output,"w")
+    #today = dt.now().date()
+    page = generate_page(
+        head = generate_head(title),
+        body = generate_body(header = header, paragraphs = paragraphs))
+    print(page, file=fp)
+    fp.close()
+
+
+today = dt.now().date()
+save_page(
+    title = "Гороскоп на сегодя",
+header = "что день" + str(today) + "Готовит",
+paragraphs = generated_prophecies(),)
